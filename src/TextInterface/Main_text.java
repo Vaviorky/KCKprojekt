0f7e9a305c412519b66807349b066a9bb99f4765
Vaviorky <danieloss15@gmail.com>
2016-10-11 10:52:03 +0200

    Project is ready to work with it. I think.

---
+++ src/TextInterface/Main_text.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TextInterface;
+
+/**
+ *
+ * @author Vaviorky
+ */
+public class Main_text {
+
+    public static void main(String[] args) {
+        
+    }
+}
