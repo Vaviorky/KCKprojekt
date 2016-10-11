1bc3684dea6b69dce25adb0938453018058b6ae2
Vaviorky <danieloss15@gmail.com>
2016-10-11 09:57:29 +0200

    Przygotowanie do projektu

---
+++ src/kck_menager/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kck_menager;
+
+/**
+ *
+ * @author Vaviorky
+ */
+// main Class
+public class Main {
+    public static void main(String[] args) {
+        Main_test.test3();
+    }
+}
