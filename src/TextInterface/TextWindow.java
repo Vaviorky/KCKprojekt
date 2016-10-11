2b939694cab33d615622fa73f01526b0c5926565
Vaviorky <danieloss15@gmail.com>
2016-10-11 14:52:33 +0200

    Zrobiono jakis prosty interfejs

--- src/TextInterface/TextWindow.java
+++ src/TextInterface/TextWindow.java
@@ -8,12 +8,10 @@ package TextInterface;
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.gui.*;
 import com.googlecode.lanterna.gui.component.Button;
-import com.googlecode.lanterna.gui.component.EmptySpace;
 import com.googlecode.lanterna.gui.component.Label;
 import com.googlecode.lanterna.gui.component.Panel;
 import com.googlecode.lanterna.gui.component.Table;
 import com.googlecode.lanterna.gui.layout.HorisontalLayout;
-import com.googlecode.lanterna.gui.layout.LayoutManager;
 import com.googlecode.lanterna.gui.layout.LinearLayout;
 import com.googlecode.lanterna.terminal.TerminalSize;
 
@@ -22,40 +20,51 @@ public class TextWindow {
     final GUIScreen guiScreen = TerminalFacade.createGUIScreen();
     final Window window = new Window("Menager plikow");
     String letWindowPath = "C:";
-    
-    
+    String rightWindowPath = "D:";
+
     public void mainWindow() {
-        window.setWindowSizeOverride(new TerminalSize(200,100));
+        window.setWindowSizeOverride(new TerminalSize(500, 200));
         window.setSoloWindow(true);
-        
-        Panel panelHolder = new Panel("Cos tam", Panel.Orientation.VERTICAL);
-        panelHolder.setPreferredSize(new TerminalSize(40, 50));
-        Panel panel = new Panel(letWindowPath);
-        panel.setLayoutManager(new HorisontalLayout());
-        panel.setPreferredSize(new TerminalSize(40, 50));
-        Button button = new Button("Test Button");
-        button.setAlignment(Component.Alignment.RIGHT_CENTER);
-        
-        panel.addComponent(button, LinearLayout.GROWS_VERTICALLY);
-        
+        Panel panelHolder = new Panel();
+        panelHolder.setLayoutManager(new HorisontalLayout());
+        panelHolder.setPreferredSize(new TerminalSize(100, 100));
+
+        Panel panelLeft = new Panel(letWindowPath);
+//        panelLeft.repaint();
+        panelLeft.setPreferredSize(new TerminalSize(100, 100));
+
+        Panel panelRight = new Panel(rightWindowPath);
+        panelRight.setLayoutManager(new HorisontalLayout());
+        panelRight.setPreferredSize(new TerminalSize(100, 100));
+
         Table table = new Table(6);
         table.setColumnPaddingSize(4);
-        
-        Component[] row = new Component[6];
-        row[0] = new Label("Plik1");
-        row[1] = new Label("Plik2");
-        row[2] = new Label("Plik3");
-        row[3] = new Label("Plik4");
-        row[4] = new Label("Plik5");
-        row[5] = new Label("Plik6");
+
+        Component[] row = new Component[2];
+        row[0] = new Label("Nazwa");
+        row[1] = new Label("Typ");
         table.addRow(row);
-        panel.addComponent(table);
-        
-        window.addComponent(panel);
-        window.addComponent(new EmptySpace());
-        
-        
-        
+        row = new Component[1];
+        row[0] = new Label("...");
+        table.addRow(row);
+        row = new Component[2];
+        row[0] = new Label("windows.exe");
+        row[1] = new Label("Trojan");
+        table.addRow(row);
+        panelLeft.addComponent(table, LinearLayout.GROWS_HORIZONTALLY);
+        Button button = new Button("HEHEHE");
+        panelLeft.addComponent(button);
+        Button button1 = new Button("HEHEHE");
+        panelLeft.addComponent(button1);
+        panelRight.addComponent(button1);
+        panelRight.addComponent(button1);
+        panelHolder.addComponent(panelLeft);
+        panelHolder.addComponent(panelRight);
+        Panel menu = new Panel("MENU");
+        menu.setPreferredSize(new TerminalSize(100, 3));
+        window.addComponent(panelHolder);
+        window.addComponent(menu);
+
         guiScreen.getScreen().startScreen();
         guiScreen.showWindow(window);
         guiScreen.getScreen().stopScreen();
